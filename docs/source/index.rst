f83b30c52f212b5aebc1aa5e40149f36f48f5e73
Yuri Pieters <magejohnyjtp@gmail.com>
2019-12-02 17:50:23 +0000

    Add documentation and slightly improve MotorDriver
    Add documentation with Sphinx. Run `make html` in the docs/ folder to
    build the html docs.
    
    Also improve the interface of MotorDriver by turning various
    attributes into properties.

---
+++ docs/source/index.rst
@@ -0,0 +1,19 @@
+Welcome to yrk-oo's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: yrk_oo.motors
+    :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
--- src/yrk_oo/motors.py
+++ src/yrk_oo/motors.py
@@ -1,45 +1,94 @@
-MOTOR1 = 0x60
-MOTOR2 = 0x62
-MOTOR3 = 0x66
-MOTOR4 = 0x68
+"""Contains the MotorDriver class and some useful constants.
+
+Example::
+
+    from smbus2 import SMBus
+    with SMBus(1) as bus:
+        driver = MotorDriver(MOTOR1, bus)
+        driver.set(1)
+
+Attributes:
+    MOTOR1:
+    MOTOR2:
+    MOTOR3:
+    MOTOR4: Constants for the addresses of each motor driver on the YRK. To be
+        passed to the initialiser of MotorDriver
+"""
+
+from smbus2 import SMBus
+
+MOTOR1: int = 0x60
+MOTOR2: int = 0x62
+MOTOR3: int = 0x66
+MOTOR4: int = 0x68
 
 
 class MotorDriver:
-    def __init__(self, motor, bus):
+    """
+    Interface for a single I2C motor driver
+    """
+    def __init__(self, motor: int, bus: SMBus):
+        """Init the class with a specific I2C address and bus
+
+        Args:
+            motor_addr: I2C address of the motor controller
+            bus: An open SMBus object
+        """
         self._bus = bus
         self._motor = motor
-        self.speed = 0
-        self.brake_state = False
-        self.direction = 0
+        self._speed = 0
+        self._brake_state = False
 
-    def set(self, speed):
+    def set(self, speed: float) -> None:
+        """Set the speed of the motor
+
+        Args:
+            speed: A value between -1 and 1. If speed < 0, the motor
+                goes in reverse; if speed > 0, the motor goes forward; if
+                motor = 0, then the motor coasts.
+        """
         if speed < -1 or speed > 1:
             raise ValueError(speed)
 
-        self.speed = speed
+        self._speed = speed
 
         if speed == 0:
             in2_in1 = 0b00
-            self.direction = 0
         elif speed < 0:
             in2_in1 = 0b01
-            self.direction = -1
         elif speed > 0:
             in2_in1 = 0b10
-            self.direction = 1
     
         # number between 0x06 and 0x3f
         vset = 0x06 + int((0x3f - 0x06) * abs(speed))
         byte = (vset << 2) | in2_in1
         self._bus.write_byte_data(self._motor, 0, byte)
 
-    def brake(self):
+    def brake(self) -> None:
+        """Put the motor into the brake state"""
         in2_in1 = 0b11
         self._bus.write_byte_data(self._motor, 0, in2_in1)
 
-        self.brake_state = True
-        self.speed = 0
-        self.direction = 0
+        self._brake_state = True
+        self._speed = 0
+
+    @property
+    def speed(self) -> float:
+        """Current speed the motor is set to. -1 <= speed <= 1"""
+        return self._speed
+
+    @property
+    def direction(self) -> int:
+        """Current direction of the motor
+
+        -1 is reverse, 0 is stationary, and 1 is forward
+        """
+        return int(self._speed / abs(self._speed))
+
+    @property
+    def brake_state(self) -> bool:
+        """Boolean of the state of the brake"""
+        return self._brake_state
 
 if __name__ == "__main__":
     from smbus2 import SMBus
